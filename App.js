8c5740ef6aa4c2e010d4d98b807b7abf2c5af606
David Oliver <davooliver1964@gmail.com>
2020-06-24 22:00:50 -0400

    working on navigation

--- App.js
+++ App.js
@@ -1,84 +1,49 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import Nav from './src/Nav';
-import Generate from './src/Generate';
-import ListItem from './src/listItem';
-import Input from './src/input';
-import PickerComponent from './src/picker';
-import ModalComponent from './src/modal';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import AppOrig from './src/AppOrig';
 
-export default class App extends Component {
-    state = {
-        nameOfApp: `Davo's awesome app`,
-        random: [42, 21, 666]
-    }
-
-    onAddRandom = () => {
-        const random = Math.floor(Math.random() * 100) + 1;
-        this.setState(prevState => {
-            return {
-                random: [...prevState.random, random]
-            }
-        })
-    }
-
-    onItemDelete = (position) => {
-        const newArray = this.state.random.filter((item, index) => {
-            return position != index;
-        })
-        this.setState({
-            random: newArray
-        })
-    }
-
-    render() {
-        return (
-            <ScrollView
-                style={{width:'100%'}}
-                // onContentSizeChange={ (w,h)=> alert(w+', '+h)}
-                // onMomentumScrollBegin={ ()=> alert()}
-                // onMomentumScrollEnd={ ()=> alert('end')}
-            >
-                <View style={styles.container}>
-
-                    <Input />
-
-                    <Nav name={this.state.nameOfApp} />
-                    <View style={styles.basicView}>
-                        <View style={styles.basicView}>
-                            <Text style={styles.basicText}>Saluton Mondo</Text>
-                        </View>
-                    </View>
-                    <Generate add={this.onAddRandom} />
-                    <ListItem items={this.state.random}
-                        delete={this.onItemDelete} />
-                </View>
-                <PickerComponent/>
-                <ModalComponent/>
-            </ScrollView>
-        )
-    };
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button
+                title="Original App"
+                onPress={() => navigation.navigate('Details')}
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-        width: '100%',
-        paddingTop: 50,
-        marginBottom: 5
-    },
-    basicView: {
-        backgroundColor: 'green',
-        width: '100%',
-        marginBottom: 5,
-    },
-    basicText: {
-        fontSize: 20,
-        color: '#fff',
-        textAlign: 'center',
-        padding: 20
-    }
-});
+function DetailsScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <AppOrig />
+            <Button
+                title="go to Details... again"
+                onPress={() => navigation.push('Details')}
+            />
+            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Button
+                title="Go back to first screen in stack"
+                onPress={() => navigation.popToTop()}
+            />
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
---
+++ src/AppOrig.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, ImageBackground } from 'react-native';
+import Nav from './Nav';
+import Generate from './Generate';
+import ListItem from './listItem';
+import Input from './input';
+import PickerComponent from './picker';
+import ModalComponent from './modal';
+import Expectations from './assets/images/expectations.jpg';
+import 'react-native-gesture-handler';
+
+export default class AppOrig extends Component {
+    state = {
+        nameOfApp: `Davo's awesome app`,
+        random: [42, 21, 666]
+    }
+
+    onAddRandom = () => {
+        const random = Math.floor(Math.random() * 100) + 1;
+        this.setState(prevState => {
+            return {
+                random: [...prevState.random, random]
+            }
+        })
+    }
+
+    onItemDelete = (position) => {
+        const newArray = this.state.random.filter((item, index) => {
+            return position != index;
+        })
+        this.setState({
+            random: newArray
+        })
+    }
+
+    render() {
+        return (
+            <ScrollView
+                style={{ width: '100%' }}
+                // onContentSizeChange={ (w,h)=> alert(w+', '+h)}
+                // onMomentumScrollBegin={ ()=> alert()}
+                // onMomentumScrollEnd={ ()=> alert('end')}
+            >
+
+                <ImageBackground
+                    source={ Expectations }
+                    style={styles.pic}
+                    resizeMode="cover"
+                    // onLoadEnd={ () => alert('loading finished')}
+                    />
+                <Image
+                    source={{ url: 'https://picsum.photos/200' }}
+                    style={styles.pic}
+                    resizeMode="cover"
+                    // onLoadEnd={ () => alert('loading finished')}
+                />
+
+                <View style={styles.container}>
+                    <Input />
+                    <Nav name={this.state.nameOfApp} />
+                    <View style={styles.basicView}>
+                        <View style={styles.basicView}>
+                            <Text style={styles.basicText}>Saluton Mondo</Text>
+                        </View>
+                    </View>
+                    <Generate add={this.onAddRandom} />
+                    <ListItem items={this.state.random}
+                        delete={this.onItemDelete} />
+                </View>
+
+                <PickerComponent />
+                <ModalComponent />
+            </ScrollView>
+        )
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        width: '100%',
+        paddingTop: 50,
+        marginBottom: 5
+    },
+    basicView: {
+        backgroundColor: 'green',
+        width: '100%',
+        marginBottom: 5,
+    },
+    basicText: {
+        fontSize: 20,
+        color: '#fff',
+        textAlign: 'center',
+        padding: 20
+    },
+    pic: {
+        width:'100%',
+        height:500,
+        marginTop:10,
+        marginBottom:5
+    }
+});
